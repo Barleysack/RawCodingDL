d0789fe9ce188db65830667097fe4e219d1bfdfe
Barleysack <hookypooky@naver.com>
2021-07-17 23:35:14 +0900

    Pandas_2 Lesson done. Need Additional Reviews_ Unsaved several lines

--- Pre-Course_04_Pandas.py
+++ Pre-Course_04_Pandas.py
@@ -62,6 +62,52 @@ print(s)
 #데이터프레임: describe, unique, 
 #각자 카테고리에 뭐가 있나 싶을때 enum/dict/unique로 처리 가능.
  
+#판다스는 엑셀 모판을 만드는 것 뿐.. 
+#1.Groupby : 통계치 . split,apply, combine 
 
 
+#hierarchical index-unstack
 
+#Group으로 묶인 데이터를 matrix 형태로 변환해줌
+#hierarchical index-swaplevel
+#인덱스 레벨을 변경 가능
+#hierarchical index-operations
+#인덱스 레벨을 기준으로 기본 연산 수행 가능 .
+#get_group: 키 값 받아서 정보 추출 가능...
+#grouped: aggregation? 람다도 받을 수 있다. 특정 컬럼에 여러개의 function을 apply 하는 것 또한 가능하다. 
+#score = lambda x : (x.max())
+#grouped.transform(): 메소드 내부 함수에 따라 내부 정보에 통으로 식 또는 함수를 걸 수 있는듯. #다만 자주 안쓴다... 
+#filter() 역시 내부 boolean 값에 따라서 필터링이 가능하다. 
+#인덱스 내 boolean 인덱스로도 입력이 가능한 것이다. 
+
+
+#agg는 하나의 컬럼/타입에서도 다양한 데이터를 뽑아낼 수 있다는 장점이 있다. 
+#피벗 테이블 또한 만들어낼 수 있다. df.pivot_table
+#두 컬럼에 교차 테이블을 만들어낼때 : pd.crosstab.
+#ipynb파일 확인할 것. 
+"""#case_ipcr_필독!!!"""
+
+
+#웬만한 데이터는 다 전처리를 해야 한다...
+#Merge: 두개의 데이터를 하나로 합침. 
+#키 값을 지정해주고 합침. pd.merge(df_a,df_b,on='subject_id')
+#기본적인 merge는 Inner Join. 
+
+#두 데이터 프레임이 컬럼이름이 다른 경우 left_on-'subject_id' 또는 right_on='subject_id' 가능..
+#필요에 따라 Full Join, Inner Join, Left Join, Right Join 기본은 inner지만 how='' 에 넣어서 변경 가능.(left,right,full...outer,inner,,,)
+#Index based Join: 잘 안쓰는 방식. 둘 다 인덱스를 살려야 할 경우이다. 
+
+#DB Persistence. 데이터 로딩 시 db connection 기능을 제공함.
+import sqlite3
+
+conn = sqlite3.connect("./data/예시.db")
+cur = conn.cursor()
+
+cur.execute("select * from 테이블 limit 5;")
+results = cur.fetchall()
+print(results)
+#뭐 이런 식으로..?
+#엑셀로 바로 변환 또한 가능하다. 
+#TO PICKLE 또한 가능하다. 
+
+ 
